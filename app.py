c74684feee6a7cf8dcec0864664dcff3fb4d8d89
Scott Haluck <sjhaluck@gmail.com>
2014-07-30 14:29:52 -0700

    added several routes, converted routes, and debug mode (which is awesome)

--- app.py
+++ app.py
@@ -6,13 +6,46 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
-
 # define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World!"
+	return "Hello, World!?!?!?!?"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+# converted routes
+# integer
+@app.route("/integer/<int:value>")
+def type(value):
+	print value + 1
+	return "correct"
+
+# float
+@app.route("/float/<float:value>")
+def type2(value):
+	print value + 1
+	return "correct"
+
+# path
+@app.route("/path/<path:value>")
+def type3(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "scott":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
